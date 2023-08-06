39d5d01e3efea5cdba2a81f156155b492dd6ed22
euphyfzhang <98727789+euphyfzhang@users.noreply.github.com>
2023-08-05 22:06:48 -0400

    Update streamlit_app.py
    add more menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit
 
-streamlit.title("My parent's New Healthy Dinner")
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title("My parent's New Healthy Dinner")\
+
+streamlit.header('Breakfast Favorite')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocade Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
