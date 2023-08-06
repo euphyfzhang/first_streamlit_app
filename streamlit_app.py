ee1ed97c2ab2f7adb78a10e9046fb107dc210b77
euphyfzhang <98727789+euphyfzhang@users.noreply.github.com>
2023-08-05 22:38:59 -0400

    Update streamlit_app.py
    change word

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.header('Breakfast Favorite')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocade Toast')
+streamlit.text('🥑🍞 Avocade and Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
